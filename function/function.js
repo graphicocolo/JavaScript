c64ad10bbed2844fc86198a2264e4f10438ea670
高橋礼 <graphicocolo@gmail.com>
2023-03-25 15:50:01 +0900

    関数

--- function/function.js
+++ function/function.js
@@ -4,7 +4,7 @@
  */
 
 /*
-関数の定義
+  関数の定義
  */
 function greet(name) {
   console.log(`Hello, ${name}!`);
@@ -14,7 +14,7 @@ greet("John"); // Hello, John!
 greet("Mary"); // Hello, Mary!
 
 /*
-return 文
+  return 文
  */
 function func4() {
   console.log('最初の処理');
@@ -24,7 +24,7 @@ function func4() {
 func4();
 
 /*
-アロー関数
+  アロー関数
  */
 const displayMessage = () => {
   console.log('Hello World!');
@@ -57,7 +57,7 @@ const func3 = (a) => {
 }
 
 /*
-コールバック関数
+  コールバック関数
  */
 // コールバック関数を受け取る関数の定義
 function myFunc1(callback) {
@@ -86,3 +86,102 @@ myFunc2(myCallback2);
 myFunc2(() => {
   console.log('無名関数2です');
 });
+
+/*
+  即時関数
+ */
+// 書き方 その1
+(function() {
+  console.log('書き方 その1 - function による即時関数');
+})(); // 終端で () をつけて作成した無名関数をその場で呼び出している
+
+(() => {
+  console.log('書き方 その1 - アロー関数による即時関数');
+})();
+// 書き方 その2
+(function() {
+  console.log('書き方 その2 - function による即時関数');
+}());
+// アロー関数では以下の記述はできない（エラー）
+// (() => {
+//   console.log('アロー関数による即時関数');
+// }());
+
+// 引数を持たせることもできる
+(function (param1, param2) {
+  console.log(`${param1}${param2}`);
+}('foo', 'baz'));
+((num1, num2) => {
+  console.log(num1 + num2);
+})(3, 5);
+
+/*
+  無名関数
+ */
+const greeting = function() {
+  console.log("Hello, world!");
+}
+greeting();
+
+/*
+  関数のスコープ
+ */
+const funcScope = () => {
+  var val1 = '変数val1';
+  const val2 = '変数val2';
+  let val3 = '変数val3';
+}
+// console.log(`${val1}${val2}${val3}`); // エラー
+
+/*
+  関数のスコープと変数の巻き上げ
+ */
+function funcHoisting() {
+  // ここに変数宣言のみが巻き上げられる
+  // 変数宣言前に変数を参照しているが
+  // エラーにはならず undefined となる
+  console.log(`1回目${hoisting}`);
+  // 代入はそのままの位置に残る
+  var hoisting = '関数内における変数の巻き上げ';
+}
+funcHoisting(); // undefined
+
+/*
+  関数のデフォルト引数
+ */
+const displayGreet = (name = 'Guest') => {
+  console.log(`Hello, ${name}!`);
+}
+displayGreet(); // "Hello, Guest!"
+displayGreet('John'); // "Hello, John!"
+
+/*
+  関数の残余引数
+ */
+const sum = (...numbers) => {
+  let total = 0;
+  for (let number of numbers) {
+    total += number;
+  }
+  return total;
+}
+console.log(sum(1, 2, 3)); // 6
+console.log(sum(4, 5, 6, 7)); // 22
+// 通常の仮引数と組み合わせて使う
+const displayDivide = (first, ...others) => {
+  console.log(first);
+  console.log(others);
+}
+displayDivide('Ken', 'Mike', 'John');
+// Ken
+// (2) ['Mike', 'John']
+
+/*
+  関数とスプレッド構文
+ */
+const displaySum = (num1, num2) => console.log(num1 + num2);
+const array = [1, 2];
+// 普通に配列の値を渡す場合
+displaySum(array[0], array[1]); // 3
+// スプレッド構文を用いた場合
+displaySum(...array); // 3
